b460966f7c01852d8d14aff40986440add35486f
mekarls <mekarls@umich.edu>
2020-11-23 15:19:43 -0500

    test 2- git pull

--- api_code.py.py
+++ api_code.py.py
@@ -1,5 +1,10 @@
-#hello
-print('hello world')
-#this is a git pull test
-lst = ['barb', 'barb', 'angel']
-print(len(lst))
+# SI 206 Final Project - STE(a)Migos
+# Names: Michelle Karls, Selin Fidan, Julia Couch
+#twitter ids: @realDonaldTrump => 25073877    @joebiden => 939091
+# Twitter account ID’S   tweeterid.com
+
+
+import requests
+import json
+import os
+import unittest
