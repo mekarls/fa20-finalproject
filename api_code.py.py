489d58431a814782409bf9221209b971c7a8e7d8
mekarls <mekarls@umich.edu>
2020-11-24 09:44:03 -0500

    code outline

--- api_code.py.py
+++ api_code.py.py
@@ -8,3 +8,26 @@ import requests
 import json
 import os
 import unittest
+
+# ------------------TUESDAY, NOV 24------------------
+# step 1: get access to twitter api
+# 
+# step 2: define function to pull ~10 tweets from a given location
+# 
+# step 3: in same function, save tweet data to a csv file 
+# [twitter id, twitter handle, date, time, tweet length, approx. lat (of given location), approx. long (of given location)]
+# step 4: create database (database name: API Data)
+# 
+# step 5: in a second function, write tweet data from file into db table 1 (Tweets)
+
+
+
+# ------------------WEDNESDAY, NOV 25------------------
+# step 1: get access to weather api
+# 
+# step 2: define function that selects date, time, location for each tweet from table 1
+# 
+# step 3: input date, time, location into api, write resulting weather into csv file for each tweet
+# 
+# step 4: in a second function, write weather data from file into db table 2 (Weather)
+
