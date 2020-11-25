56ac7dfa9509cb0c466ac5ff3524b6e74a154aef
mekarls <mekarls@umich.edu>
2020-11-25 12:58:48 -0500

    inputted updated datetime

--- api_code.py.py
+++ api_code.py.py
@@ -7,6 +7,7 @@ import os
 import unittest
 import sqlite3
 import urllib.request
+from datetime import datetime
 
 # ------------------TUESDAY, NOV 24------------------
 # step 1: create database
@@ -22,9 +23,9 @@ def iss_position():
     base_url = 'http://api.open-notify.org/iss-now.json'
     req = requests.get(base_url)
     data = req.json()
-    time = data['timestamp']
-    lat = data['iss_position']['latitude']
-    long = data['iss_position']['longitude']
+    time = data['timestamp'] #str
+    lat = data['iss_position']['latitude'] #str
+    long = data['iss_position']['longitude'] #str
     # return {"time": time, "lat": lat, "long": long}
     return f'{time},{lat},{long}'
 
@@ -51,20 +52,24 @@ def write_iss_csv():
 
 def create_iss_table(cur, conn):
     """make sure to commit new data"""
-    # cur.execute('''CREATE TABLE IF NOT EXISTS 'ISS_Data_1' 
-    # ('timestamp' TEXT, 'latitude' TEXT, 'longitude' TEXT)''')
+    cur.execute('''DROP TABLE IF EXISTS 'ISS_Data_1'''')
+    cur.execute('''CREATE TABLE 'ISS_Data_1' 
+    ('unix' TEXT, 'latitude' TEXT, 'longitude' TEXT)''')
     
-    #cur.execute('''CREATE TABLE IF NOT EXISTS 'ISS_Data_2' 
-    #('timestamp' TEXT, 'latitude' TEXT, 'longitude' TEXT)''')
+    cur.execute('''DROP TABLE IF EXISTS 'ISS_Data_2'''')
+    cur.execute('''CREATE TABLE 'ISS_Data_2' 
+    ('unix' TEXT, 'latitude' TEXT, 'longitude' TEXT)''')
 
-    #cur.execute('''CREATE TABLE IF NOT EXISTS 'ISS_Data_3' 
-    #('timestamp' TEXT, 'latitude' TEXT, 'longitude' TEXT)''')
+    cur.execute('''DROP TABLE IF EXISTS 'ISS_Data_3'''')
+    cur.execute('''CREATE TABLE 'ISS_Data_3' 
+    ('unix' TEXT, 'latitude' TEXT, 'longitude' TEXT)''')
 
-    # cur.execute('''CREATE TABLE IF NOT EXISTS 'ISS_Data_4' 
-    # ('timestamp' TEXT, 'latitude' TEXT, 'longitude' TEXT)''')
+    cur.execute('''DROP TABLE IF EXISTS 'ISS_Data_4'''')
+    cur.execute('''CREATE TABLE  'ISS_Data_4' 
+    ('unix' TEXT, 'latitude' TEXT, 'longitude' TEXT)''')
 
 def insert_iss_data(cur, conn):    
-    # with open('iss_pos.csv', 'r') as fhand:
+    with open('iss_pos.csv', 'r') as fhand:
     #with open('iss_pos2.csv', 'r') as fhand:
     #with open('iss_pos3.csv', 'r') as fhand:
     # with open('iss_pos4.csv', 'r') as fhand:
@@ -85,34 +90,56 @@ def insert_iss_data(cur, conn):
             lats.append(float(lat))
             longs.append(float(long))
 
-            # cur.execute('INSERT INTO ISS_Data_1 (timestamp, latitude, longitude) VALUES (?, ?, ?)', (time, lat, long))
-            #cur.execute('INSERT INTO ISS_Data_2 (timestamp, latitude, longitude) VALUES (?, ?, ?)', (time, lat, long))
-            #cur.execute('INSERT INTO ISS_Data_3 (timestamp, latitude, longitude) VALUES (?, ?, ?)', (time, lat, long))
-            # cur.execute('INSERT INTO ISS_Data_4 (timestamp, latitude, longitude) VALUES (?, ?, ?)', (time, lat, long))
+            cur.execute('INSERT INTO ISS_Data_1 (unix, latitude, longitude) VALUES (?, ?, ?)', (time, lat, long))
+            #cur.execute('INSERT INTO ISS_Data_2 (unix, latitude, longitude) VALUES (?, ?, ?)', (time, lat, long))
+            #cur.execute('INSERT INTO ISS_Data_3 (unix, latitude, longitude) VALUES (?, ?, ?)', (time, lat, long))
+            # cur.execute('INSERT INTO ISS_Data_4 (unix, latitude, longitude) VALUES (?, ?, ?)', (time, lat, long))
             conn.commit()
 
         t = sum(times)/float(len(times))
+        t_update = datetime.utcfromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S') #returns date, time
         la = round(sum(lats)/float(len(lats)), 4)
         lo = round(sum(longs)/float(len(longs)), 4)
         
         cur.execute('''CREATE TABLE IF NOT EXISTS 'Avg_ISS'
-        (id INTEGER PRIMARY KEY, avg_time NUMBER, avg_lat NUMBER, avg_long NUMBER)''')
+        (id INTEGER PRIMARY KEY, avg_unix NUMBER, date TEXT, time TEXT, avg_lat NUMBER, avg_long NUMBER)''')
 
-        # cur.execute('INSERT INTO Avg_ISS (id, avg_time, avg_lat, avg_long) VALUES (?, ?, ?, ?)', (1, t, la, lo))
-        #cur.execute('INSERT INTO Avg_ISS (id, avg_time, avg_lat, avg_long) VALUES (?, ?, ?, ?)', (2, t, la, lo))
-        #cur.execute('INSERT INTO Avg_ISS (id, avg_time, avg_lat, avg_long) VALUES (?, ?, ?, ?)', (3, t, la, lo))
-        # cur.execute('INSERT INTO Avg_ISS (id, avg_time, avg_lat, avg_long) VALUES (?, ?, ?, ?)', (4, t, la, lo))
+        cur.execute('INSERT INTO Avg_ISS (id, avg_time, avg_lat, avg_long) VALUES (?, ?, ?, ?)', (1, t, t_update[0], t_update[1], la, lo))
+        #cur.execute('INSERT INTO Avg_ISS (id, avg_time, avg_lat, avg_long) VALUES (?, ?, ?, ?)', (2, t, t_update[0], t_update[1], la, lo))
+        #cur.execute('INSERT INTO Avg_ISS (id, avg_time, avg_lat, avg_long) VALUES (?, ?, ?, ?)', (3, t, t_update[0], t_update[1], la, lo))
+        # cur.execute('INSERT INTO Avg_ISS (id, avg_time, avg_lat, avg_long) VALUES (?, ?, ?, ?)', (4, t, t_update[0], t_update[1], la, lo))
         conn.commit()
      
+# def time_converstion(cur, conn):
+#     cur.execute('SELECT avg_time FROM Avg_ISS')
+#     times = cur.fetchall()
+#     update_times = []
+#     t = [''.join(str(time)) for time in times]
+#     tim = [ti.strip('(),') for ti in t]
+#     print(tim)
+    # update_times = [datetime.utcfromtimestamp(float(ts)).strftime('%Y-%m-%d %H:%M:%S') for ts in tim]
+    # for time in update_times:
+    #     time = time.split()
+    #     d = time[0]
+    #     t = time[1]
+        # cur.execute('ALTER TABLE Avg_ISS ADD date TEXT')
+        # cur.execute('ALTER TABLE Avg_ISS ADD time TEXT')
+        # cur.execute('''UPDATE Avg_ISS SET 
+        # date = d
+        # time = t
+        # FROM Avg_ISS
+        # WHERE id = 1''')
+        # conn.commit()
+    pass
 
 def create_weather_table(cur, conn):
     """make sure to commit new data"""
     pass
 
 def weather(params):
-    api_key = '97H6P669AZU5PIG16JBC5N4ES'
-    base_url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/history/'
-    [location]/[date1]/[date2]?key=YOUR_API_KEY 
+    # api_key = '97H6P669AZU5PIG16JBC5N4ES'
+    # base_url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/history/'
+    # [location]/[date1]/[date2]?key=YOUR_API_KEY 
     # req = requests.get(base_url, )
     #[key]/[latitude],[longitude],[time]'
     # select data from Avg_ISS to input into requests
@@ -140,10 +167,9 @@ def main():
     # pass
     # Database and Tables
     cur, conn = setUpDatabase('API_Data.db')
-    write_iss_csv()
-    create_iss_table(cur, conn)
-    insert_iss_data(cur, conn)
-    
+    # write_iss_csv()
+    # create_iss_table(cur, conn)
+    # insert_iss_data(cur, conn)
     # create_weather_table(cur, conn)
 
     # create_daylight_table(cur, conn)
