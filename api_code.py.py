d0465ce64c6cc40996d0f4571fa7ed8e67b11e01
mekarls <mekarls@umich.edu>
2020-12-01 17:33:52 -0500

    daylight api

--- api_code.py.py
+++ api_code.py.py
@@ -74,41 +74,62 @@ def weather(cur, conn):
     data1 = cur.fetchall()
 
     data2 = []
-    for i in data1[1]:
+    for i in data1[:1]:
         lat = i[0]
         lon = i[1]
         dat = i[2]
         r = requests.get(base_url + '?locations=' + lat + ',' + lon + '/' + "&dayStartTime=" + dat + "&dayEndTime" + dat + "?key=" + api_key)
-        a = r.text()
-     
-        
+        a = r.json()
+        print(type(a))
 
-  
 
- 
-   
+'''             things to ask at office hours:
+1) how to create one table with location_id and location name (as part of weather tables) that we can then use as foreign keys for the daylight table
+2) how do we deal with the json response we're getting from the weather api
+3) how do we pull from the database in one file to make the visualizations if it's created in a different file
+'''  
 
 def create_daylight_table(cur, conn):
     """make sure to commit new data"""
-    pass
+    
+    cur.execute("DROP TABLE IF EXISTS 'Daylight'")
+    cur.execute('''CREATE TABLE IF NOT EXISTS 'Daylight' 
+    ('sunrise' TEXT, 'sunset' TEXT, 'solar_noon' TEXT, 'day_length' TEXT)''')
+    #{'results': {'sunrise': '1:14:49 PM', 'sunset': '9:39:33 PM', 'solar_noon': '5:27:11 PM', 'day_length': '08:24:44', 'civil_twilight_begin': '12:37:37 PM', 'civil_twilight_end': '10:16:46 PM', 'nautical_twilight_begin': '11:56:55 AM', 'nautical_twilight_end': '10:57:28 PM', 'astronomical_twilight_begin': '11:18:02 AM', 'astronomical_twilight_end': '11:36:21 PM'}, 'status': 'OK'}
 
-def daylight(params):
-    #select data from ISS_Data to input into requests
-    pass
+    cur.execute('SELECT latitude, longitude, date FROM ISS_Data')
+    data = cur.fetchall()
+    # print(data)
+    for d in data: #make sure to do 25 at a time
+        lat, long, date = d[0], d[1], d[2]
+    
+    #important to note for calculations- given in UTC (coordinated universal time)
+        base_url = 'https://api.sunrise-sunset.org/json'
+        req = requests.get(f'{base_url}?lat={lat}&lng={long}&date={date}')
+        resp = req.json()
+        resp_dict = resp['results']
+
+        rise, set, solar, length = resp_dict['sunrise'], resp_dict['sunset'], resp_dict['solar_noon'], resp_dict['day_length']
+       
+        cur.execute('INSERT INTO Daylight (sunrise, sunset, solar_noon, day_length) VALUES (?, ?, ?, ?)', (rise, set, solar, length))
+        conn.commit()
+
+    # pass
+
+
 
 
 
 def main():
-    
-    # pass
+
     # Database and Tables
     cur, conn = setUpDatabase('API_Data.db')
    
     #create_iss_table(cur, conn)
-    #create_weather_table(cur, conn)
-    #create_weather_table(cur, conn)
+
     # create_weather_table(cur, conn)
-    weather(cur, conn)
-    # create_daylight_table(cur, conn)
+    # weather(cur, conn)
+    create_daylight_table(cur, conn)
+
 if __name__ == '__main__':
     main()
